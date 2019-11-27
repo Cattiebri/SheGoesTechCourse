d97133a31f6cf9b14e5a843a63ce0a7bb3ee7c99
Cattiebri <ssantucis@gmail.com>
2019-11-27 20:42:18 +0200

    test

--- lecture16/src/com/accenture/lecture16/WarriorApp.java
+++ lecture16/src/com/accenture/lecture16/WarriorApp.java
@@ -6,16 +6,18 @@ import java.util.List;
 public class WarriorApp {
 
 	public static void main(String[] args) {
-////		Warrior ernie = new Warrior("Ernie", 64, 11);
-//		Warrior bobby = new Warrior ("Bobby", 12, 22);
-//		Warrior jimmy = new Warrior ("Jimmy", 32, 9);
-//		Warrior tommy = new Warrior ("Tommy", 87, 4);
-//		
+//		Warrior ernie = new Warrior("Ernie", 64, 11);
+		Warrior bobby = new Warrior ("Bobby", 12, 22);
+		Warrior jimmy = new Warrior ("Jimmy", 32, 9);
+		Warrior tommy = new Warrior ("Tommy", 87, 4);
+		
+		System.out.println("Test");
+		
 //		List<Warrior> warriors = new ArrayList<>();
 //		warriors.add(bobby);
 //		warriors.add()
 //		
 //		ernie.fight(bobby);
-//	}
+
 	}
 }
